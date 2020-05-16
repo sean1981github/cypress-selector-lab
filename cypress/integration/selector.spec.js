2172cbeb4ca34be95e2126ca382d0acfeb92d57c
Sean <sean1981@gmail.com>
2020-05-16 16:44:04 +0800

    [Sean] Cypress selector lab

--- cypress/integration/selector.spec.js
+++ cypress/integration/selector.spec.js
@@ -26,4 +26,25 @@ describe("Cypress Selector", () => {
   it("chaining selectors", () => {
     cy.get(".topnav").contains("CSS");
   });
+
+  it("find by id bootstrap", () => {
+    cy.get(".topnav").contains("BOOTSTRAP");
+    //cy.get("w3-bar").contains("BOOTSTRAP");
+  });
+
+  it("find by text THE WORLD'S LARGEST WEB DEVELOPER SITE", () => {
+    cy.contains("THE WORLD'S LARGEST WEB DEVELOPER SITE");
+  });
+
+  it("find by attribute Search W3Schools", () => {
+    cy.get('[title = "Search W3Schools"]');
+  });
+
+  it("find by combinations of button and class for next button", () => {
+    cy.get("a.w3-right.w3-btn").first();
+  });
+
+  it("find by class w3-example", () => {
+    cy.get(".w3-example").find("h3").contains("Example");
+  });
 });
